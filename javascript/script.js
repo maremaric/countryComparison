d133bf24481c38c6a5062a0a13183416e50f8e6f
maremaric <markoreus888@gmail.com>
2021-06-08 12:54:36 +0200

    vezba

--- javascript/script.js
+++ javascript/script.js
@@ -1,30 +1,51 @@
 
-
 const apiUrl = 'https://restcountries.eu/rest/v2/all';
 
     async function getData() 
     {
         const response = await fetch(apiUrl);
         const data = await response.json();
-    
+        
         console.log(data);
 
         if(data.length > 0) 
         {
-            var temp = "";
-
+            
             for(let i = 0; i < data.length; i++) {
-
-                temp += `<input id='${data[i].name}' type='checkbox' value='${data[i].population}' />`;
-                temp += "<tr>";
-                temp += "<td>" + data[i].name + "</td>" + "</tr>";
+                let temp = document.createElement('div');
+                temp.innerHTML = "<div id=" + data[i].name + " class='js-country-element'><input id=" + data[i].name + " type='checkbox' value=" + data[i].population + "><p>" + data[i].name + "</p></div>";
+                // temp += `<input id='${data[i].name}' type='checkbox' value='${data[i].population}' />`;
+                // temp += "<tr>";
+                // temp += "<td>" + data[i].name + "</td>" + "</tr>";
+                document.getElementById("data").appendChild(temp);
             }
-
-            document.getElementById("data").innerHTML = temp;
-
         }
-
     }
+        
+    function getAllCheckedCountries() {
+        var checkbox = document.querySelectorAll('input[type="checkbox"]:checked');
+
+            var arrayCountries = [];
+
+            checkbox.forEach((e) => {
+
+                let countryName = e.id;
+                let countryValue = Number(e.value);
+                console.log('El val: ', e.value);
+
+                let obj = {
+                    'name': countryName,
+                    'value': countryValue
+                }
+
+                arrayCountries.push(obj);
+
+            });
+            console.log('Checked arr countries: ', arrayCountries);
+            
+            return arrayCountries;
+    }
+    
     getData();
     
     let btnCompare = document.getElementById("compare");
@@ -34,78 +55,75 @@ const apiUrl = 'https://restcountries.eu/rest/v2/all';
     
     btnCompare.addEventListener('click', () => {
 
-        var checkbox = document.querySelectorAll('input[type="checkbox"]:checked');
-
-        var arrayCountries = [];
-
-        checkbox.forEach((e) => {
-
-            let countryName = e.id;
-            let countryValue = Number(e.value);
-
-            let obj = {
-                'name': countryName,
-                'value': countryValue
-            }
-
-            arrayCountries.push(obj);
-
-        });
+        arrayCountries = getAllCheckedCountries();
 
 
-        var maxValue = arrayCountries[0].value;
+        let maxValue = {
+            'name': arrayCountries[0].name,
+            'value': arrayCountries[0].value
+        }
+        
         
         for(let i = 0; i < arrayCountries.length; i++) 
         {
-            if(arrayCountries[i].value > maxValue) maxValue = arrayCountries[i].value;
+            if(arrayCountries[i].value > maxValue.value) maxValue = arrayCountries[i];
         };
 
 
-        var minValue = arrayCountries[0].value;
+        let minValue = {
+            'name': arrayCountries[0].name,
+            'value': arrayCountries[0].value
+        }
 
         for(let i = 0; i < arrayCountries.length; i++) 
         {
-            if(arrayCountries[i].value < minValue) minValue = arrayCountries[i].value;
+            if(arrayCountries[i].value < minValue.value) minValue = arrayCountries[i];
         };
        
 
-        let difference = maxValue - minValue;
+        let difference = maxValue.value - minValue.value;
 
 
-        let maxValueFormat = numbro(maxValue).format({thousandSeparated: true});
-        let minValueFormat = numbro(minValue).format({thousandSeparated: true});
+        let maxValueFormat = numbro(maxValue.value).format({thousandSeparated: true});
+        let minValueFormat = numbro(minValue.value).format({thousandSeparated: true});
         let differenceFormat = numbro(difference).format({thousandSeparated: true});
         
     
-        max.innerText = `Most populared country is: [Country name] with ${maxValueFormat} inhabitants`;
-        min.innerText = `Most least populared country is: [Country name] with ${minValueFormat} inhabitants`;
+        max.innerText = `Most populared country is: ${maxValue.name} with ${maxValueFormat} inhabitants`;
+        min.innerText = `Most least populared country is: ${minValue.name} with ${minValueFormat} inhabitants`;
         diff.innerText = `Difference of population is: ${differenceFormat} inhabitants`;
 
-
+        function getAllCountriesPropertiesInSeperatedArrs(allCheckedCountries) {
+            let objToRet = {countryNames: [], countryValues: []};
+            allCheckedCountries.forEach(country => {
+                objToRet.countryNames.push(country.name);
+                objToRet.countryValues.push(country.value);
+            });
+            console.log(objToRet)
+            return objToRet;
+        }
         // chart
         const ctx = document.getElementById('chart').getContext('2d');
+        let allCheckedCountries = getAllCheckedCountries();
+        let seperatedArrs = getAllCountriesPropertiesInSeperatedArrs(allCheckedCountries);
         const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Red', 'Blue'],
+            labels: seperatedArrs.countryNames,
             datasets: [{
                 label: 'Country',
-                data: [maxValue,minValue],
+                data: seperatedArrs.countryValues,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(75, 192, 192, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(75, 192, 192, 1)'
                 ],
                 borderWidth: 1
             }]
@@ -125,39 +143,21 @@ const apiUrl = 'https://restcountries.eu/rest/v2/all';
 
     
 
-    // Search bar
+    // // Search bar
     const searchBar = document.getElementById("searchBar");
-    const data = document.getElementById("data");
-     
-    // Search country.json
+  
     const searchCountries = async searchText => {
-        const res = await fetch(apiUrl);
-        const countries = await res.json();
-
-        // Get matches to current text input
-        let matches = countries.filter(state => {
-            const regex = new RegExp(`^${searchText}`, 'gi');
-            console.log(regex);
-            return state.name.match(regex);
-        });
-
-        outputHtml(matches);
+        const countryElements = document.querySelectorAll('.js-country-element');
+        // console.log('Country elements: ', countryElements);
+        countryElements.forEach(country => {
+            console.log('Country id: ', country.id)
+            console.log('Country style: ', !(country.id.toLowerCase().includes(searchText.toLowerCase())));
+            if ((country.id.toLowerCase().includes(searchText.toLowerCase()))) {
+                country.style.display = 'block';
+            } else {
+                country.style.display = 'none';
+            }
+        })
     };
-
-    // Show result 
-    const outputHtml = matches => 
-    {
-        if(matches.length > 0) 
-        {
-            const html = matches.map(match => 
-                `<div>
-                    <h4>${match.name}</h4>
-                </div>`    
-            ).join('');
-
-            data.innerHTML = html;
-        }
-    }
-
     searchBar.addEventListener('input', () => searchCountries(searchBar.value));
 
